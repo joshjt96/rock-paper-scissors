3f281e940f3363ea02379422d44c516e554e2131
Josh Talley <jtdev84@gmail.com>
2022-03-31 11:36:36 +0100

    Add if else to playRound and refactor this code

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 function game() {
-    playRound();
+    for (let i = 0; i < 5; i++) {
+      playRound();
+    };
 };
 
 function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
-    return "You lose! " + playerSelection + " beats " + computerSelection
+    if (playerSelection === 'rock' && computerSelection === 'Paper') {
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection);
+    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+    } else if (playerSelection === 'paper' && computerSelection === 'Paper') {
+        console.log('Tie! You both chose ' + playerSelection);
+    } else if (playerSelection === 'rock' && computerSelection === 'Rock') {
+        console.log('Tie! You both chose ' + playerSelection);
+    } else if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
+        console.log('Tie! You both chose ' + playerSelection);
+    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection);
+    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection);
+    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+    }  
 };
 
 function playerChoice() {
-    const userPrompt = prompt("Choose rock, paper or scissors.");
+    const userPrompt = prompt('Choose rock, paper or scissors.');
     return userPrompt;
 };
 
@@ -17,3 +37,20 @@ function computerChoice() {
     let rps = ['Rock', 'Paper', 'Scissors'];
     return rps[Math.floor(Math.random() * rps.length)]
 };
+
+// Refactored code
+// function playRound() {
+//     const playerSelection = playerChoice();
+//     const computerSelection = computerChoice();
+//     if ((playerSelection === 'paper' && computerSelection === 'Rock') ||
+//     (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+//     (playerSelection === 'scissors' && computerSelection === 'Paper')) {
+//         console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+//     } else if (playerSelection === computerSelection) {
+//         console.log('Tie! You both chose ' + playerSelection);
+//     } else if ((playerSelection === 'paper' && computerSelection === 'Scissors') ||
+//     (playerSelection === 'rock' && computerSelection === 'Paper') ||
+//     (playerSelection === 'scissors' && computerSelection === 'Rock')) {
+//         console.log('You lose! ' + computerSelection + ' beats ' + playerSelection);
+//     }
+// }
